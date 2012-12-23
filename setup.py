131ac38c0b78bdd6d0e5d23aac2a0aaa2008db96
Martin Peeters _martin.peeters@affinitic.be_ <martin.peeters@affinitic.be>
2012-12-23 18:28:01 +0100

    - Updates the buildout and adds the files for jenkins. ##

--- setup.py
+++ setup.py
@@ -13,32 +13,39 @@ long_description = (
     open('CHANGES.txt').read()
     + '\n')
 
-setup(name='gites.shop',
-      version=version,
-      description="Boutique for Gites",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Plone",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['gites', ],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.PloneGetPaid',
-          'getpaid.ogone'
-      ], extras_require={'test': ['plone.app.testing']})
+setup(
+    name='gites.shop',
+    version=version,
+    description="Boutique for Gites",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      "Environment :: Web Environment",
+      "Framework :: Plone",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 2.6",
+      "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://svn.plone.org/svn/collective/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['gites', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.PloneGetPaid',
+        'gites.core',
+        'getpaid.ogone'
+    ],
+    extras_require={
+        'test': [
+            'unittest2',
+            'plone.app.testing',
+    ]})
