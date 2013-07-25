95faf90c1f1d0ff183a59181eccf887393789170
Laurent Lasudry <lasudry@gmail.com>
2013-07-25 14:14:22 +0200

    Change schema to reflect checkout addresses logic (required fields)
    PEP8

--- src/gites/shop/interfaces.py
+++ src/gites/shop/interfaces.py
@@ -1,44 +1,45 @@
 from zope import schema
-from zope.i18nmessageid import MessageFactory
 from gites.locales import GitesMessageFactory as _
 from zope.interface import Interface
 from zope.viewlet.interfaces import IViewletManager
 
-class IGDWOrderDetailsManager( IViewletManager ):
+
+class IGDWOrderDetailsManager(IViewletManager):
         """ viewlet manager for a single order
             """
 
-class IGDWAdminOrderManager( IViewletManager ):
+
+class IGDWAdminOrderManager(IViewletManager):
         """ viewlet manager for a single order
             """
 
-class IBillingAddress( Interface ):
+
+class IBillingAddress(Interface):
     """ where to bill
     """
-    ship_same_billing = schema.Bool( title = _(u"Same as shipping address"), required=False)
+    ship_same_billing = schema.Bool(title=_(u"Same as shipping address"),
+                                    required=False)
 
-    bill_first_line = schema.TextLine( title = _(u"First Line"), required=False)
-    bill_second_line = schema.TextLine( title = _(u"Second Line"), required=False )
-    bill_city = schema.TextLine( title = _(u"City"), required=False)
-    bill_country = schema.Choice(title = _(u"Country"),
-                                 vocabulary = "getpaid.countries",
-                                 default=u'BE',
-                                 required=False)
-    bill_postal_code = schema.TextLine( title = _(u"Zip Code"),
+    bill_first_line = schema.TextLine(title=_(u"First Line"))
+    bill_second_line = schema.TextLine(title=_(u"Second Line"),
                                        required=False)
-    vat_number = schema.TextLine(title = _(u"VAT Number"),
+    bill_city = schema.TextLine(title=_(u"City"))
+    bill_country = schema.Choice(title=_(u"Country"),
+                                 vocabulary="getpaid.countries",
+                                 default=u'BE')
+    bill_postal_code = schema.TextLine(title=_(u"Zip Code"))
+    vat_number = schema.TextLine(title=_(u"VAT Number"),
                                  required=False)
 
 
-class IShippingAddress( Interface ):
+class IShippingAddress(Interface):
     """ where to send goods
     """
-    ship_first_line = schema.TextLine( title = _(u"First Line"))
-    ship_second_line = schema.TextLine( title = _(u"Second Line"),
+    ship_first_line = schema.TextLine(title=_(u"First Line"))
+    ship_second_line = schema.TextLine(title=_(u"Second Line"),
                                        required=False)
-    ship_city = schema.TextLine( title = _(u"City"))
-    ship_country = schema.Choice( title = _(u"Country"),
-                                    vocabulary = "getpaid.countries",
+    ship_city = schema.TextLine(title=_(u"City"))
+    ship_country = schema.Choice(title=_(u"Country"),
+                                 vocabulary="getpaid.countries",
                                  default=u'BE')
-    ship_postal_code = schema.TextLine( title = _(u"Zip Code"))
-
+    ship_postal_code = schema.TextLine(title=_(u"Zip Code"))
