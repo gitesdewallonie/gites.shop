ea07a9bf05f0f7eb46aebca2185bae79e6658c45
Laurent Lasudry <lasudry@gmail.com>
2013-07-31 20:53:39 +0200

    Fix untranslatable columns titles by using their name (#5264)

--- src/gites/shop/browser/cart.py
+++ src/gites/shop/browser/cart.py
@@ -1,10 +1,19 @@
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from getpaid.core import interfaces
 from Products.PloneGetPaid.browser.cart import ShoppingCartActions
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from Products.PloneGetPaid.i18n import _
-from Products.PloneGetPaid.browser.cart import CartFormatter
+from Products.PloneGetPaid.browser.cart import (CartFormatter,
+                                                ShoppingCartListing,
+                                                lineItemURL,
+                                                formatLinkCell,
+                                                lineItemPrice,
+                                                lineItemTotal)
 from zope.formlib import form
+from Products.CMFPlone.i18nl10n import utranslate
 from Products.CMFCore.utils import getToolByName
+
+from zc.table import column
 
 
 # override template
@@ -27,3 +36,27 @@ class GDWShoppingCartActions(ShoppingCartActions):
         portal = getToolByName(self.context, 'portal_url').getPortalObject()
         url = portal.absolute_url() + '/@@getpaid-checkout-wizard'
         return self.request.RESPONSE.redirect(url)
+
+
+class GDWShoppingCartListing(ShoppingCartListing):
+    """
+    """
+    template = ZopeTwoPageTemplateFile('templates/cart-listing.pt')
+
+    columns = [
+        column.SelectionColumn(lambda item: item.item_id, name="selection"),
+        column.FieldEditColumn(_(u"Quantity"), 'edit', interfaces.ILineItem['quantity'], lambda item: item.item_id, name="Quantity"),
+        column.GetterColumn(title=_(u"Name"), name="Name", getter=lineItemURL, cell_formatter=formatLinkCell),
+        column.GetterColumn(title=_(u"Price"), name="Price", getter=lineItemPrice),
+        column.GetterColumn(title=_(u"Total"), name="Total", getter=lineItemTotal),
+       ]
+
+    def update(self):
+        for column in self.columns:
+            if column.name == 'selection':
+                continue
+            if hasattr(column, 'title'):
+                column.title = utranslate(domain='plonegetpaid',
+                                          msgid=column.name,
+                                          context=self.request)
+        super(ShoppingCartListing, self).update()
