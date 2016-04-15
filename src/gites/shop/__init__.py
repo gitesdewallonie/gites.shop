4ff8202eb6e64d4a1736bf4b8770af2516f024ac
Jean-Francois Roche <jfroche@affinitic.be>
2016-04-15 17:36:54 +0200

    change zc datetime widget default format - refs #6405

--- src/gites/shop/__init__.py
+++ src/gites/shop/__init__.py
@@ -1,5 +1,9 @@
 # -*- extra stuff goes here -*-
 import member
 
+from zc.datetimewidget.datetimewidget import DateWidget
+DateWidget._format = '%d/%m/%Y'
+
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
